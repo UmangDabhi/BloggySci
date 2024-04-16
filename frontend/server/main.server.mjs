5f01ca462acdda0c491bc0b2e3db717f3a016293
Umang Dabhi <ud30104@gmail.com>
2024-04-16 18:02:30 +0530

    Version 2

--- frontend/server/main.server.mjs
+++ frontend/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{b as a}from"./chunk-2A7MO6EQ.mjs";import"./chunk-RIEN45O7.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{b as a}from"./chunk-7Y5RPDYP.mjs";import"./chunk-B33PISSI.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
